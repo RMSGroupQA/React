181bd46b8f1224b2a645a86fbbe079c345f77c26
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-03-28 10:16:52 +0000

    Image Container Css Added

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <br/><br/>
+        <br /><br />
         <h2>Welcome
         </h2>
-        <ImageMapper src={"https://www.vertex42.com/calendars/images/2020-calendar-with-holidays.png"} width={500}/>
+        <div className="imagecontainer">
+          <ImageMapper src={"https://www.vertex42.com/calendars/images/2020-calendar-with-holidays.png"} width={500} />
+        </div>
         <br /><br />
       </div>
     );
